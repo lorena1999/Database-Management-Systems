4c3d19e2e1de8cd6feb1b082803e23043af9ae64
lorena1999 <lorena.marginean@yahoo.com>
2020-07-31 14:09:42 +0300

    CRUD operations on a database

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Practic2R
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvTickets = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.dgvMatches = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTickets)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatches)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTickets
+            // 
+            this.dgvTickets.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTickets.Location = new System.Drawing.Point(63, 236);
+            this.dgvTickets.Name = "dgvTickets";
+            this.dgvTickets.RowTemplate.Height = 24;
+            this.dgvTickets.Size = new System.Drawing.Size(410, 150);
+            this.dgvTickets.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Matches";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tickets";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(505, 287);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(75, 23);
+            this.updateBtn.TabIndex = 3;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // dgvMatches
+            // 
+            this.dgvMatches.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatches.Location = new System.Drawing.Point(63, 38);
+            this.dgvMatches.Name = "dgvMatches";
+            this.dgvMatches.RowTemplate.Height = 24;
+            this.dgvMatches.Size = new System.Drawing.Size(410, 150);
+            this.dgvMatches.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 411);
+            this.Controls.Add(this.dgvMatches);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvTickets);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTickets)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatches)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvTickets;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.DataGridView dgvMatches;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Practic2R
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection dbConn;
+        SqlDataAdapter daMatches, daTickets;
+        DataSet ds;
+        SqlCommandBuilder cbTickets;
+        BindingSource bsTickets, bsMatches;
+
+        // By clicking the update button, the changes are propagated at the level of database
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            daTickets.Update(ds, "Tickets");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bsMatches = new BindingSource();
+            bsTickets = new BindingSource();
+
+            // bind the data grid views to the binding sources
+            dgvMatches.DataSource = bsMatches;
+            dgvTickets.DataSource = bsTickets;
+
+            // this is the connection string that we use in order to connect to our database
+            dbConn = new SqlConnection("Data Source=LM_PC\\LORESQL;Initial Catalog=Practic2M;Integrated Security=True");
+
+            // instantiate dataSet 
+            ds = new DataSet();
+
+            /// instantiate dataAdapters
+            daMatches = new SqlDataAdapter("SELECT * FROM Matches", dbConn);
+            daTickets = new SqlDataAdapter("SELECT * FROM Tickets", dbConn);
+
+            // instantiate the command builder
+            cbTickets = new SqlCommandBuilder(daTickets);
+
+            // call fill method on data adapters in order to retreive all the matches and the tickets
+            daMatches.Fill(ds, "Matches");
+            daTickets.Fill(ds, "Tickets");
+
+            // Matches is the parent table and Tickets is the child table
+            DataRelation dr = new DataRelation("MatchesTickets",
+                ds.Tables["Matches"].Columns["MatchID"],
+                ds.Tables["Tickets"].Columns["MatchID"]);
+
+            ds.Relations.Add(dr);
+
+            bsMatches.DataSource = ds;
+            bsMatches.DataMember = "Matches";
+
+            bsTickets.DataSource = bsMatches;
+            bsTickets.DataMember = "MatchesTickets";
+
+        }
+    }
+}
